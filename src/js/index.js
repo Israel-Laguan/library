25e069da5cd15ac9e7589a0b40fdbc98cfad7c84
Israel-Laguan <israellaguan@gmail.com>
2020-01-28 11:29:45 -0500

    [tests] Test: Testing for closing Issue #1
    What?:
    Added all the tests for Issue #1
    
    Breaking Changes?

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 
 let myLibrary = []
 
-/*
+/**
  * Constructor for Books belonging the Library
  * @param {String} title
  * @param {String} author
@@ -25,5 +25,36 @@ const addBookToLibrary = (title, author, pages, read) => {
   return myLibrary
 }
 
-console.log(addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 293, false))
-console.log(addBookToLibrary('Other', 'Other', 293, true)[0].title)
+const issue1 = {
+  myLibraryIsArray: (libraryArray) => Array.isArray(libraryArray),
+  addBookToLibraryIsFunction: (obj)=>!!(obj && obj.constructor && obj.call && obj.apply),
+  addBookToLibraryAddToArray: ()=>{
+    const before = myLibrary.length;
+    addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 293, false)
+    return myLibrary.length === before + 1
+  },
+  addBookToLibraryStoresValues: (title)=>{
+    addBookToLibrary(title, 'author', 293, false)
+    return myLibrary[myLibrary.length-1].title === title
+  },
+  checkTitleStored: (author) =>{
+    addBookToLibrary('Testing', author, 293, false)
+    return myLibrary[myLibrary.length-1].author === author
+  },
+  checkPagesStored: (pages) =>{
+    addBookToLibrary('Testing', 'Testing', pages, false)
+    return myLibrary[myLibrary.length-1].pages === pages
+  },
+  checkReadStored: (read) =>{
+    addBookToLibrary('Testing', 'Testing', 293, read)
+    return myLibrary[myLibrary.length-1].read === read
+  },
+}
+
+console.log(issue1.myLibraryIsArray(myLibrary))
+console.log(issue1.addBookToLibraryIsFunction(addBookToLibrary))
+console.log(issue1.addBookToLibraryAddToArray())
+console.log(issue1.addBookToLibraryStoresValues('Testing 1'))
+console.log(issue1.checkTitleStored('Testing 2'))
+console.log(issue1.checkPagesStored(100))
+console.log(issue1.checkReadStored(true))

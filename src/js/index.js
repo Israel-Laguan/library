7d02f60e38b7fd9c6a45d2a17d062666c7f61fdb
Israel-Laguan <israellaguan@gmail.com>
2020-01-28 11:12:44 -0500

    [feat] JS: add Books constructor and add2Library method
    What?:
    - Added myLibrary to work as storage
    - Added a book constructor with values and method
    - Added a function to add books to storage
    
    Breaking Changes? no

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+let myLibrary = []
+
+/*
+ * Constructor for Books belonging the Library
+ * @param {String} title
+ * @param {String} author
+ * @param {Number} pages
+ * @param {Boolean} read
+ */
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = () =>
+    `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? 'read' : 'not read'
+    }.`
+}
+const addBookToLibrary = (title, author, pages, read) => {
+  const book = new Book(title, author, pages, read)
+  myLibrary.push(book)
+  return myLibrary
+}
+
+console.log(addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 293, false))
+console.log(addBookToLibrary('Other', 'Other', 293, true)[0].title)

6f7cb2ed2ecd117d9f0ecb95b094c2a553a868f8
Israel-Laguan <israellaguan@gmail.com>
2020-01-28 23:08:33 -0500

    [feat] HTML+JS: Add Book is working, issue #4
    What?:
    
    Breaking Changes?

--- src/js/index.js
+++ src/js/index.js
@@ -18,8 +18,12 @@ function Book(title, author, pages, read) {
     this.read ? 'read' : 'not read'
   }.`;
   this.render = () => `
-  <td><span>Title:</span>${this.title}</td>
-  <td><span>Author:</span>${this.author}</td>
+  <td><span>Title:</span><a href="https://www.google.com/?q=%22${
+  this.title
+}+by+${this.author}%22&as_filetype=pdf">${this.title}</a></td>
+  <td><span>Author:</span><a href="https://www.google.com/?q=%22${
+  this.author
+}%22">${this.author}<a/></td>
   <td><span>Pages:</span>${this.pages}</td>
   <td><span>Read?:</span>${this.read ? 'Read' : 'Not read'}</td>
   <td><button>Erase</button></td>
@@ -40,14 +44,48 @@ export const addBookToLibrary = (title, author, pages, read) => {
   return myLibrary;
 };
 
-
-window.addEventListener('load', () => {
-  addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 293, false);
-  addBookToLibrary('The Lord of The Rings', 'J.R.R. Tolkien', 646, true);
+/**
+ * render myLibrary on HTML
+ */
+export const showList = () => {
   const list = document.getElementById('list');
   myLibrary.forEach((book, id) => {
     list.innerHTML += `
     <tr key=${id}>${book.render()} </tr>
     `;
   });
+};
+
+const validateForm = (formData) => {
+  if (!formData.title) return { error: 'Title is empty' };
+  if (!formData.author) return { error: 'Author is empty' };
+  return {};
+};
+
+window.addEventListener('load', () => {
+  showList();
+});
+
+const newBookForm = document.getElementById('new-book-form');
+newBookForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const data = {
+    // @ts-ignore
+    title: newBookForm.title.value,
+    // @ts-ignore
+    author: newBookForm.author.value,
+    // @ts-ignore
+    pages: newBookForm.pages.value,
+    // @ts-ignore
+    read: newBookForm.read.checked,
+  };
+  const { error } = validateForm(data);
+  if (error) {
+    document.getElementById('alert').innerText = error;
+    return;
+  }
+  addBookToLibrary(data.title, data.author, data.pages, data.read);
+  showList();
+  // @ts-ignore
+  newBookForm.reset();
 });

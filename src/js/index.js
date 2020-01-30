c95f0123b92b5eba11c114bd1767df0953f2df40
Israel-Laguan <israellaguan@gmail.com>
2020-01-30 14:06:19 -0500

    [refactor] Global: multifile
    What?:
    
    Breaking Changes?

--- src/js/index.js
+++ src/js/index.js
@@ -1,82 +1,15 @@
 // @ts-check
-export const myLibrary = [];
+import { showLibrary, addBook, showNotification } from './views.js';
 
-/**
- * Constructor for Books belonging the Library
- * @param {String} title
- * @param {String} author
- * @param {String} isbn
- * @param {Number} pages
- * @param {Boolean} read
- */
-function Book(title = 'Test Title', author = 'Test Author', isbn = '978-3-16-148410-0', pages = 296, read = false) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.isbn = isbn;
-  this.info = () => `${this.title} by ${this.author}, ISBN ${this.isbn}, ${this.pages} pages, ${
-    this.read ? 'read' : 'not read'
-  }.`;
-  this.render = () => `
-  <td><span>Title:</span><a href="https://www.google.com/?q=%22${
-  this.title
-}+by+${this.author}%22&as_filetype=pdf">${this.title}</a></td>
-  <td><span>Author:</span><a href="https://www.google.com/?q=%22${
-  this.author
-}%22">${this.author}<a/></td>
-  <td><span>ISBN:</span>${this.isbn}</td>
-  <td><span>Pages:</span>${this.pages}</td>
-  <td><span>Read?:</span>${this.read ? 'Read' : 'Not read'}</td>
-  <td><button>Erase</button></td>
-  `;
-}
-
-/**
- * Adds a Book to the Library
- * @param {String} title
- * @param {String} author
- * @param {String} isbn
- * @param {Number} pages
- * @param {Boolean} read
- * @returns {Array} Library List with Books
- */
-export const addBookToLibrary = (title, author, isbn, pages, read) => {
-  const book = new Book(title, author, isbn, Number(pages), read);
-  myLibrary.push(book);
-  return myLibrary;
-};
-
-/**
- * render myLibrary on HTML
- */
-export const showList = () => {
-  if (myLibrary.length === 0) {
-    document.getElementById('notification').innerHTML = `
-    <div class="notice" id="notice">
-      <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
-      <p><b>Aware!:</b> No books on Library! ⬇ Add here</p>
-    </div>
-    `;
-    return;
-  }
-  const list = document.getElementById('list');
-  myLibrary.forEach((book, id) => {
-    list.innerHTML += `
-    <tr key=${id}>${book.render()}</tr>
-    `;
-  });
-};
+window.addEventListener('load', () => {
+  showLibrary();
+});
 
 const validateForm = (formData) => {
   if (!formData.title) return { error: 'Title is empty' };
   if (!formData.author) return { error: 'Author is empty' };
   return {};
 };
-
-window.addEventListener('load', () => {
-  showList();
-});
 
 const newBookForm = document.getElementById('new-book-form');
 newBookForm.addEventListener('submit', event => {
@@ -95,22 +28,11 @@ newBookForm.addEventListener('submit', event => {
   };
   const { error } = validateForm(data);
   if (error) {
-    document.getElementById('notification').innerHTML = `
-    <div class="alert" id="alert">
-      <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
-      <p><b>Danger!:</b> ${error}</p>
-    </div>
-    `;
-    return;
+    showNotification('warning', error);
+    return [];
   }
-  addBookToLibrary(data.title, data.author, data.isbn, data.pages, data.read);
-  showList();
+  const result = addBook(data.title, data.author, data.isbn, data.pages, data.read);
   // @ts-ignore
-  newBookForm.reset();
-  document.getElementById('notification').innerHTML = `
-  <div class="notice" id="notice">
-    <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
-    <p><b>Success!:</b> Book Added!</p>
-  </div>
-  `;
+  if (!result.length < 1) newBookForm.reset();
+  return result;
 });
